7c8e39c35112bc27261f4b6b12a775e34bbea06a
Asraf Ali <mdashraf9135@gmail.com>
2026-02-12 14:38:33 +0530

    last commit

--- title.php
+++ title.php
@@ -10,7 +10,8 @@
         'doctor-wise-schedule' => 'Doctor Wise Schedule',
         'appointment-reschedule-cancel' => 'Reschedule or Cancel Appointment',
         'online-appointment' => 'Online Appointment',
-        'attendance' => 'Staff Attendance'
+        'attendance' => 'Staff Attendance',
+        'leave-management' => 'Leave Management'
     ];
 
     $title = isset($titles[$page]) ? $titles[$page] : 'Home Page';
